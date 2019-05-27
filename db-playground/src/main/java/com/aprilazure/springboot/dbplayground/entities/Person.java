2e309b8f61869457aee8d74e023e1bea6b7ac281
april.azure@gmail.com <april.azure@gmail.com>
2019-05-27 09:29:50 +0800

    minor modification

--- db-playground/src/main/java/com/aprilazure/springboot/dbplayground/entities/Person.java
+++ db-playground/src/main/java/com/aprilazure/springboot/dbplayground/entities/Person.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.UUID;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,11 +16,14 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Person implements Serializable{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Id
 	private UUID id; 
 	private String name;
 	private Date birthDate;
